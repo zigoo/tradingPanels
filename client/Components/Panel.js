4fe421435655dd829d4e576ef384c4deedf7a909
pnalewajka <przemyslaw.nalewajka@domeny.pl>
2018-04-23 21:59:19 +0200

    map thtough panel component

--- client/Components/Panel.js
+++ client/Components/Panel.js
@@ -1,37 +1,48 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import styles from "../Styles/styles";
+import { ArrowSvg, InsideText } from "../Helpers/helpers";
 
-const newLineSpan = {
-	style: {
-		display: "block"
-	}
-};
 
 class Panel extends PureComponent {
 	render() {
 		const { currencyPair, buy, sell } = this.props;
 
+		const buyStringValue = buy.toString().padEnd(7, 0)
+		const sellStringValue = sell.toString().padEnd(7, 0)
 		const buyCurr = currencyPair.substring(0, 3);
 		const sellCurr = currencyPair.substring(4, 7);
 
+		const CurrencyHeader = props => (
+			<div
+				{...props}
+				style={styles.pairHeader}>
+					{currencyPair}
+			</div>)
+		
 		return (
 			<div style={styles.wrapper}>
-				<div style={styles.pairHeader}>
-					{currencyPair}
-				</div>
+				<CurrencyHeader/>
 				<div style={styles.content}>
-					<div style={styles.sellPanel}>
-						<span {...newLineSpan}>
-							Sell {sellCurr}
-						</span>
-						<span>{sell}</span>
+					<div style={styles.panel}>
+						<ArrowSvg>
+							<InsideText sell top>
+								Sell {sellCurr}
+							</InsideText>
+							<InsideText sell>
+								{sellStringValue}
+							</InsideText>
+						</ArrowSvg>
 					</div>
-					<div style={styles.buyPanel}>
-						<span {...newLineSpan}>
-							Buy {buyCurr}
-						</span>
-						<span>{buy}</span>
+					<div style={{...styles.panel, ...styles.buyPanel }}>
+						<ArrowSvg right>
+							<InsideText top right>
+								Buy {buyCurr}
+							</InsideText>
+							<InsideText buy right>
+								{buyStringValue}
+							</InsideText>
+						</ArrowSvg>
 					</div>
 				</div>
 			</div>
@@ -41,8 +52,8 @@ class Panel extends PureComponent {
 
 Panel.propTypes = {
 	currencyPair: PropTypes.string.isRequired,
-	buy:PropTypes.number.isRequired,
-	sell:PropTypes.number.isRequired
-}
+	buy: PropTypes.number.isRequired,
+	sell: PropTypes.number.isRequired
+};
 
 export default Panel;
