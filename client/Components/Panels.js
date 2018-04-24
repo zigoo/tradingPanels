a429dd55f1d9fb64c3e58840050158d9da1130ba
pnalewajka <przemyslaw.nalewajka@domeny.pl>
2018-04-24 11:51:12 +0200

    fix data for up/down indicator to make sense

--- client/Components/Panels.js
+++ client/Components/Panels.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import Panel from "./Panel";
 
 const currenciesPairs = [
-	{ pair: "USD CHF", buy: 0.99143, sell: 0.99043 },
-	{ pair: "GBP USD", buy: 1.28495, sell: 1.2836 },
-	{ pair: "GBP CHF", buy: 1.27378, sell: 1.27147 },
-	{ pair: "EUR SEK", buy: 9.6320, sell: 9.6055 },
-	{ pair: "USD JPY", buy: 110.467, sell: 110.417 },
-	{ pair: "EUR JPY", buy: 120.589, sell: 120.491 }
+  {"pair":"USD CHF", "buy":0.99143, "sell":0.99343},
+  {"pair":"GBP USD", "buy":1.28495, "sell":1.2836},
+  {"pair":"GBP CHF", "buy":1.27378, "sell":1.29147},
+  {"pair":"EUR SEK", "buy":9.632, "sell":9.6055},
+  {"pair":"USD JPY", "buy":110.467, "sell":111.417},
+  {"pair":"EUR JPY", "buy":120.589, "sell":120.491}
 ];
 
 class Panels extends Component {
