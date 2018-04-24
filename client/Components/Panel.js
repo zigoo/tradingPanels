4f48f625109e038fd88de1266d2f1f016d843cb0
pnalewajka <przemyslaw.nalewajka@domeny.pl>
2018-04-24 12:04:56 +0200

    add up/down indicator; clean

--- client/Components/Panel.js
+++ client/Components/Panel.js
@@ -3,47 +3,53 @@ import PropTypes from "prop-types";
 import styles from "../Styles/styles";
 import { ArrowSvg, InsideText } from "../Helpers/helpers";
 
-
 class Panel extends PureComponent {
 	render() {
 		const { currencyPair, buy, sell } = this.props;
 
-		const buyStringValue = buy.toString().padEnd(7, 0)
-		const sellStringValue = sell.toString().padEnd(7, 0)
+		const buyStringValue = buy.toString().padEnd(7, 0);
+		const sellStringValue = sell.toString().padEnd(7, 0);
 		const buyCurr = currencyPair.substring(0, 3);
 		const sellCurr = currencyPair.substring(4, 7);
 
 		const CurrencyHeader = props => (
 			<div {...props} style={styles.pairHeader}>
-					{currencyPair}
-			</div>)
-		
+				{currencyPair}
+			</div>
+		);
+
+		const buyOverSell = buy > sell;
 		return (
 			<div style={styles.wrapper}>
-			<div style={{border: "1px #ccc solid", height: "100%"}}>
-				<CurrencyHeader/>
-				<div style={styles.content}>
-					<div style={styles.panel}>
-						<ArrowSvg>
-							<InsideText sell top>
-								Sell {sellCurr}
-							</InsideText>
-							<InsideText sell>
-								{sellStringValue}
-							</InsideText>
-						</ArrowSvg>
+				<div style={{ border: "1px #ccc solid", height: "100%" }}>
+					<CurrencyHeader />
+					<div style={styles.content}>
+						<div style={styles.panel}>
+							<ArrowSvg>
+								<InsideText sell top>
+									Sell {sellCurr}
+								</InsideText>
+								<InsideText sell>{sellStringValue}</InsideText>
+							</ArrowSvg>
+						</div>
+						<div
+							style={{
+								...styles.triangle,
+								borderBottomColor: buyOverSell ? "green" : "red",
+								transform: !buyOverSell ? "rotate(180deg)" : null
+							}}
+						/>
+						<div style={{ ...styles.panel, ...styles.buyPanel }}>
+							<ArrowSvg right>
+								<InsideText top right>
+									Buy {buyCurr}
+								</InsideText>
+								<InsideText buy right>
+									{buyStringValue}
+								</InsideText>
+							</ArrowSvg>
+						</div>
 					</div>
-					<div style={{...styles.panel, ...styles.buyPanel }}>
-						<ArrowSvg right>
-							<InsideText top right>
-								Buy {buyCurr}
-							</InsideText>
-							<InsideText buy right>
-								{buyStringValue}
-							</InsideText>
-						</ArrowSvg>
-					</div>
-				</div>
 				</div>
 			</div>
 		);
--- client/Components/Panels.js
+++ client/Components/Panels.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import Panel from "./Panel";
 
 const currenciesPairs = [
-  {"pair":"USD CHF", "buy":0.99143, "sell":0.99343},
-  {"pair":"GBP USD", "buy":1.28495, "sell":1.2836},
+  {"pair":"USD CHF", "buy":0.99443, "sell":0.99343},
+  {"pair":"GBP USD", "buy":1.28495, "sell":1.2876},
   {"pair":"GBP CHF", "buy":1.27378, "sell":1.29147},
   {"pair":"EUR SEK", "buy":9.632, "sell":9.6055},
   {"pair":"USD JPY", "buy":110.467, "sell":111.417},
